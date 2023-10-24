3230234a6926e4b5feeb36a03d3bdb5419b41455
Faluyi <phirmzhy@gmail.com>
2023-10-24 10:59:43 +0100

    Integer values

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from db.models import *
+app = Flask(__name__)
+
+Integer_db = Integersdb()
+
+@app.post('/integer')
+def post_integer():
+    body = request.get_json()
+    curr_value = int(body['integer'])
+    prev_val = list(Integer_db.get_integer())
+    
+    if len(prev_val) == 0:
+        Integer_db.post_integer(curr_value) 
+        return jsonify({"value": curr_value})
+    
+    else:
+        prev_val_id = prev_val[0]["_id"]
+        Integer_db.replace_integer(prev_val_id, curr_value)     
+        return jsonify({"value": curr_value})
+
+@app.get('/integer')
+def get_integer():
+    item = list(Integer_db.get_integer())
+
+    return jsonify({"value": item[0]["value"]})
+
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ db/models.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId 
+
+uri = "mongodb://localhost:27017"
+client = MongoClient(uri)
+db = client['Integers_Db']
+Integers = db['Integers']
+
+
+class Integersdb:
+    def __init__(self) -> None:
+        self.collection = Integers
+        
+    def get_integer(self):
+        return self.collection.find().limit(1)
+    
+    def post_integer(self, integer: int):
+        return self.collection.insert_one({"value": integer}).inserted_id
+        
+    def replace_integer(self, prev_val_id, curr_value):
+        return self.collection.find_one_and_replace({"_id":ObjectId(prev_val_id)}, {"value": curr_value})
